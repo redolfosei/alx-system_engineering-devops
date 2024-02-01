efe82884dcc20901172fa84f3baeb35dd8486053
RedolF <redolf639@gmail.com>
2024-02-01 12:00:47 +0000

    web stack debugging 4 0 file modified

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,14 +1,12 @@
-# Increases the amount of traffic an Nginx server can handle.
+# This increases the traffice capacity of an Nginx server
 
-# Increase the ULIMIT of the default file
-exec { 'fix--for-nginx':
+exec { 'fix--for--nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-} ->
+  path    => '/usr/local/bin/:/bin'
+}
 
-# Restart Nginx
+# Restart
 exec { 'nginx-restart':
   command => 'nginx restart',
   path    => '/etc/init.d/'
 }
-
